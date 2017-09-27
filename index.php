21bca77cb71c1234c872a5aeedb64d13ef5d488b
Patrick Igwe <junniepat@gmail.com>
2017-09-27 13:36:58 +0100

    naspire

---
+++ index.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>naspire</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <!-- CSS Files -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/css/now-ui-kit.css" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="./assets/css/demo.css" rel="stylesheet" />
+</head>
+
+<body >
+    <!-- Navbar -->
+    <nav class="navbar navbar-toggleable-md bg-white mb-0 " >
+        <div class="container">
+            <div class="navbar-translate">
+             
+                <a class="navbar-brand" href="http://demos.Junniepat-.com/now-ui-kit/index.html" rel="tooltip" title="designed by junniepat. Coded by Junniepat " data-placement="bottom" target="_blank">
+                   Naspire
+                </a>
+            </div>
+            <div class="input-group form-group-no-border">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-search"></i>
+                                    </span>
+                                    <input class="form-control" placeholder="Search..." type="text">
+                                </div>
+            <div class="collapse navbar-collapse justify-content-end hidden-md-down" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
+                <ul class="navbar-nav ">
+                                       <li class="nav-item dropdown">
+                       
+                                                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <p>
+                                                        <img src="assets/img/avatar.jpg" height="30">
+                                                        WafiJoe
+                                                    </p>
+                                                </a>
+                                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                                    <a class="dropdown-item" href="#">Action</a>
+                                                    <a class="dropdown-item" href="#">Another action</a>
+                                                    <a class="dropdown-item" href="#">Something else here</a>
+                                                </div>
+                                            </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
+     <nav class="navbar navbar-toggleable-md bg-primary p-0 mb-0 ">
+                                <div class="container">
+                                    <button class="navbar-toggler navbar-toggler-left mt-3" type="button" data-toggle="collapse" data-target="#example-naspire" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-bar bar1"></span>
+                                        <span class="navbar-toggler-bar bar2"></span>
+                                        <span class="navbar-toggler-bar bar3"></span>
+                                    </button>
+                                    
+                                    <div class="collapse navbar-collapse" id="example-naspire" data-nav-image="assets/img/blurred-image-1.jpg">
+                                        <ul class="navbar-nav">
+                                              <li class="nav-item dropdown hidden-md-up">
+                       
+                                                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <p>
+                                                        <img src="assets/img/avatar.jpg" height="30">
+                                                        WafiJoe
+                                                    </p>
+                                                </a>
+                                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                                    <a class="dropdown-item" href="#">Action</a>
+                                                    <a class="dropdown-item" href="#">Another action</a>
+                                                    <a class="dropdown-item" href="#">Something else here</a>
+                                                </div>
+                                            </li>
+                                         <li class="nav-item ">
+                                                <a class="nav-link" href="#">
+                                                    <p>Dose of Naspire</p>
+                                                </a>
+                                            </li>
+                                            <li class="nav-item p-0">
+                                                <a class="nav-link" href="#">
+                                                    <p>E-learning</p>
+                                                </a>
+                                            </li>
+
+                                              <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                    <p>Bants and vents</p>
+                                                </a>
+                                            </li>
+
+                                              <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                    <p>Lessons from Balogun Market</p>
+                                                </a>
+                                            </li>
+
+                                              <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                    <p>Side Hussle Gallery</p>
+                                                </a>
+                                            </li>
+
+                                             <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                    <p>Industry Scoop</p>
+                                                </a>
+                                            </li>
+
+                                             <li class="nav-item">
+                                                <a class="nav-link" href="#">
+                                                    <p>Events</p>
+                                                </a>
+                                            </li>
+                                           
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+    <div class="wrapper" style="background:#eee;">
+      
+      
+        <div class="main p-4">
+          <div class="row">
+              <div class="col-md-3">
+                  <div class="card-block bg-white">
+                     <div class="row text-center"> <div class="col-md-3 pr-0">
+                  <img src="assets/img/avatar.jpg" height="50"></div>
+                  <div class="col-md-9 pl-1 text-center"> Jonathan Otokun<br>
+                  @wafijoe</div>
+                     </div>
+
+                  <div class="row">
+                  <div class="col-lg-4 col-md-12 ">
+                  Followers
+                  <div class="col-sm-12 text-center  text-white bg-primary">230</div>
+                  </div>
+                  
+                  <div class="col-lg-4 col-sm-12">
+                  Following
+                  <div class="col-sm-12 text-center  text-white bg-primary">2</div>
+                  </div>
+
+                  <div class=" col-lg-4 col-sm-12">
+                  messages
+                  <div class="col-sm-12 text-center text-white bg-primary">0</div>
+                  </div>
+
+                  </div>
+                  </div>
+
+                  <div class="col-md-12 card-block bg-white mt-3 mb-4">
+<div class="title mb-4">My N Tribe</div>
+
+
+<div class="row mb-2"> 
+<div class="col-md-3 pr-0">
+<img src="assets/img/avatar.jpg" height="50"></div>
+<div class="col-md-9 bg-info pt-2"> <b>Jonathan Otokun</b><br>
+<small>2 months, 2 weeks ago</small><br><br>
+selflessness is the key to greatness....
+</div>
+    </div>
+
+<div class="row mb-2"> 
+<div class="col-md-3 pr-0">
+<img src="assets/img/avatar.jpg" height="50"></div>
+<div class="col-md-9 bg-info pt-2"> <b>Jonathan Otokun</b><br>
+<small>2 months, 2 weeks ago</small><br><br>
+selflessness is the key to greatness....
+</div>
+</div>
+
+<div class="row mb-2"> 
+<div class="col-md-3 pr-0">
+<img src="assets/img/avatar.jpg" height="50"></div>
+<div class="col-md-9 bg-info pt-2"> <b>Jonathan Otokun</b><br>
+<small>2 months, 2 weeks ago</small><br><br>
+selflessness is the key to greatness....
+</div>
+</div>
+
+                  </div>
+
+              </div>
+              
+               <div class="col-md-6">
+                   <div class="row">
+                   <div class="col-md-12 card-block bg-white">
+                       <textarea class="form-control pt-4 text-black" style="font-size:1.5em;" placeholder="Get questions or Knowledge to share?"></textarea>
+                    
+                       <div class="row mt-1 pt-1">
+                       <div class="col-md-6">
+                           <input type="checkbox" id="anoy"> <label for="anoy"> Anonymous</label><br>
+                            <i class="fa fa-file-text"></i>
+                             <i class="fa fa-video-camera"></i>
+                           <i class="fa fa-music"></i>
+                            <i class="fa fa-photo"></i>
+                       </div>
+                       <div class="col-md-6 text-right"><button class="btn btn-simple pull-right"> Post Update</button>
+                       </div>
+                       </div>
+                   </div>
+<div class="col-md-9 mt-2 mb-2"><hr></div><div class="col-md-3 mt-2 mb-2">Most recent</div>
+                   </div>
+                   <div class="row p-0">
+<div class="col-md-12 p-0 mb-2">
+<div class="card-block bg-white">
+    <div class="col-md-12 mb-2 p-0">
+           <div class="row "> <div class="col-md-1 pl-0 pr-0">
+                  <img src="assets/img/avatar.jpg" height="50"></div>
+                  <div class="col-md-10"> Jonathan Wrote a new post, Backend Post<br>
+<small>1 week 1 month ago</small></div>
+                     </div>
+
+    </div>
+    <img src="assets/img/avatar.jpg" height="500" width="100%">
+</div>
+</div>
+
+<div class="col-md-12 p-0 mb-2">
+<div class="card-block bg-white">
+    <div class="col-md-12 mb-2 p-0">
+           <div class="row "> <div class="col-md-1 pl-0 pr-0">
+                  <img src="assets/img/avatar.jpg" height="50"></div>
+                  <div class="col-md-10"> Jonathan Wrote a new post, Backend Post<br>
+<small>1 week 1 month ago</small></div>
+                     </div>
+
+    </div>
+    <img src="assets/img/avatar.jpg" height="500" width="100%">
+</div>
+</div>
+
+<div class="col-md-12 p-0 mb-2">
+<div class="card-block bg-white">
+    <div class="col-md-12 mb-2 p-0">
+           <div class="row "> <div class="col-md-1 pl-0 pr-0">
+                  <img src="assets/img/avatar.jpg" height="50"></div>
+                  <div class="col-md-10"> Jonathan Wrote a new post, Backend Post<br>
+<small>1 week 1 month ago</small></div>
+                     </div>
+
+    </div>
+    <img src="assets/img/avatar.jpg" height="500" width="100%">
+</div>
+</div>
+
+<div class="col-md-12 p-0 mb-2">
+<div class="card-block bg-white">
+    <div class="col-md-12 mb-2 p-0">
+           <div class="row "> <div class="col-md-1 pl-0 pr-0">
+                  <img src="assets/img/avatar.jpg" height="50"></div>
+                  <div class="col-md-10"> Jonathan Wrote a new post, Backend Post<br>
+<small>1 week 1 month ago</small></div>
+                     </div>
+
+    </div>
+    <img src="assets/img/avatar.jpg" height="500" width="100%">
+</div>
+</div>
+                   </div>
+               
+
+               </div>
+                <div class="col-md-3">
+                    <div class="card-block bg-white" style="height:290px;">
+                        <small class="pull-right" >Advertisement</small>
+<div class="col-md-12 mt-5 pt-5">
+                        <h3 class="col-sm-12 text-center">Ad Goes Here</h3>
+</div>
+                    </div>
+                </div>
+          </div>
+
+        </div>
+       
+        <footer class="footer" data-background-color="black">
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="https://www.Junniepat-.com">
+                                Junniepat 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://presentation.Junniepat-.com">
+                                About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://blog.Junniepat-.com">
+                                Blog
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Junniepatofficial/now-ui-kit/blob/master/LICENSE.md">
+                                MIT License
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="copyright">
+                    &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>, Designed by
+                    <a href="http://www.junniepat.github.io" target="_blank">Junniepat</a>. Coded by
+                    <a href="https://www.Junniepat-.com" target="_blank">Junniepat </a>.
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<script src="./assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/tether.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="./assets/js/plugins/bootstrap-switch.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="./assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="./assets/js/now-ui-kit.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        // the body of this function is in assets/js/now-ui-kit.js
+        nowuiKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+        if ($('.section-download').length != 0) {
+            $("html, body").animate({
+                scrollTop: $('.section-download').offset().top
+            }, 1000);
+        }
+    }
+</script>
+
+</html>
